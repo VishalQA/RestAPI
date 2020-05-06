d69531d3a0c09c7cc34acd69f8143e0b9eb54590
jaju_v <jaju_v@VishalKumar-Ja.India.XoriantCorp.com>
2020-05-06 21:16:53 +0530

    ok

--- resapi/src/test/java/com/qa/test/GetAPITest.java
+++ resapi/src/test/java/com/qa/test/GetAPITest.java
@@ -56,14 +56,33 @@ public class GetAPITest extends TestBase {
 			    JSONObject responseJSON = new JSONObject(responseString);
 			    System.out.println("Response JSON from API --->" +responseJSON);
 			    
-			    //total page
+			    // single value assertion
+			    //per page
 			    String perPageValue = Testutil.getValueByJPath(responseJSON , "/per_page");
 			    System.out.println("Value of per page is ---->" +perPageValue);
 			    
-			    Assert.assertEquals(perPageValue, "6");
-//			    Assert.assertEquals(perPageValue,Integer.valueOf("6"));
+			    //Assert.assertEquals(perPageValue, "6");
+			    Assert.assertEquals(Integer.parseInt(perPageValue),6 );
 			    
-			
+			    // total value 
+			    String totalValue = Testutil.getValueByJPath(responseJSON , "/total");
+			    System.out.println("Value of total  is ---->" +totalValue);
+			    
+			    Assert.assertEquals(Integer.parseInt(totalValue),12);
+			    
+			    
+			    // get the value from JSON Array
+			    String lastname = Testutil.getValueByJPath(responseJSON, "/data[0]/last_name");
+			    String id = Testutil.getValueByJPath(responseJSON, "/data[0]/id");
+			    String avatar = Testutil.getValueByJPath(responseJSON, "/data[0]/avatar");
+			    String firstname = Testutil.getValueByJPath(responseJSON, "/data[0]/first_name");
+			    String email = Testutil.getValueByJPath(responseJSON, "/data[0]/email");
+			    
+			    System.out.println(lastname);
+			    System.out.println(id);
+			    System.out.println(avatar);
+			    System.out.println(firstname);
+			    System.out.println(email);
 			    
 			    //c. All Headers
 			     Header[] headersArray = closeableHttpResponse.getAllHeaders();
